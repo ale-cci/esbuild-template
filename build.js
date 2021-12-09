f7915b730966600f37df579b69d306441a33d49c
ale-cci <alecrd98@gmail.com>
2021-12-09 18:19:12 +0100

    feat: transpile output to es5

--- build.js
+++ build.js
@@ -3,52 +3,42 @@ import esbuild from 'esbuild'
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 import {sassPlugin} from 'esbuild-sass-plugin'
-import http from 'http'
+import babel from 'esbuild-plugin-babel'
 
 const buildOptions = {
   entryPoints: {
     'js/index': './src/js/index.js',
     'css/style': './src/css/style.scss',
   },
+  target: 'es5',
   bundle: true,
   sourcemap: true,
-  outdir: 'public/',
   color: true,
-  target: 'es5',
   minify: true,
   plugins: [
+    babel({
+      filter: /\.js$/,
+      config: {
+        presets: ['@babel/preset-env']
+      }
+    }),
     sassPlugin()
   ]
 }
 
 
-; (function main() {
-  const args = (
-    yargs(hideBin(process.argv))
-    .command('build', 'build the js and css files')
-    .command('serve', 'serve the builded application and public directory')
-    .help()
-    .demandCommand(1)
-    .strict()
-    .parse()
-  )
-
-  const [cmd] = args._
-  switch (cmd) {
-    case 'build': build(); break;
-    case 'serve': serve(); break;
-  }
-})()
-
-
 async function build() {
-  const result = await esbuild.build(buildOptions)
+  const result = await esbuild.build({
+    ...buildOptions,
+    outdir: 'build/',
+    metafile: true,
+  })
   const text = await esbuild.analyzeMetafile(result.metafile, {verbose: true})
   console.log(text)
 }
 
 async function serve() {
-  function onRequest({method, path, status, timeInMS}) {
+  function onRequest({method, path, status}) {
     console.log(` \x1b[33m [${method}] \x1b[0m (${status}) ${path}`)
   }
   const {host, port} = await esbuild.serve({
@@ -56,6 +46,7 @@ async function serve() {
     servedir: 'public',
   }, {
     ...buildOptions,
+    outdir: 'public/',
     banner: {
       js: ' (() => new EventSource("http://localhost:8082").onmessage = () => location.reload())();',
     },
@@ -63,3 +54,22 @@ async function serve() {
 
   console.log(`Server ready on http://${host}:${port}`)
 }
+
+(function main() {
+  const args = (
+    yargs(hideBin(process.argv))
+      .command('build', 'build the js and css files')
+      .command('serve', 'serve the builded application and public directory')
+      .help()
+      .demandCommand(1)
+      .strict()
+      .parse()
+  )
+
+  const [cmd] = args._
+  switch (cmd) {
+  case 'build': build(); break
+  case 'serve': serve(); break
+  }
+})()
+
